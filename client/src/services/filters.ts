841202d1ab97786165bd7468d97cb57e55d6d343
Vladislav-Evg-Sid <vlad.s200@mail.ru>
2026-08-21 12:50:36 +0500

    add filter panel for participants

--- client/src/services/filters.ts
+++ client/src/services/filters.ts
@@ -1,14 +1,32 @@
+import { getReferenceTable } from "../api/references";
 import { type CellFilter } from "../types/filters";
 
 export async function getParticipantsFilters(): Promise<CellFilter[]> {
+  const nationsTable = (await getReferenceTable("nations")).body;
+  const nationVariants: [number, string][] = nationsTable.map(({ row }) => [
+    +(row[0] ?? -1),
+    String(row[1] ?? ""),
+  ]);
+
+  const participantStatusesTable = (
+    await getReferenceTable("participant-statuses")
+  ).body;
+  const participantStatusesVariants: [number, string][] =
+    participantStatusesTable.map(({ row }) => [
+      +(row[0] ?? -1),
+      String(row[1] ?? ""),
+    ]);
+
   return [
     {
       name: "Национальность",
       type: "select",
+      variants: nationVariants,
     },
     {
       name: "Статус",
       type: "select",
+      variants: participantStatusesVariants,
     },
     {
       name: "ФИО",
